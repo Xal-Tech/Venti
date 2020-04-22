b0f7b5c20b4f85a621012e542425e750fecedd38
maurisba <mauriciobaa@alumno.hayas.edu.mx>
2020-04-22 17:10:28 -0500

    inicial commit

---
+++ new/inc/navbar.php
@@ -0,0 +1,39 @@
+<div class="navbar container-fluid fixed-bottom" style="height: 60px; background-color: #4CAF50;">
+  <div class="col pad0">
+    <center>
+      <a class="nav-link" href="miEmpresa.php"><span class="material-icons md-light">person</span></a>
+    </center>
+  </div>
+  <div class="col pad0">
+    <center>
+      <a class="nav-link" href="index.php"><span class="material-icons md-light">home</span></a>
+    </center>
+  </div>
+  <div class="col pad0">
+    <center>
+      <a class="nav-link" href="catalogo.php"><span class="material-icons md-light">view_module</span></a>
+    </center>
+  </div>
+  <div class="col pad0">
+    <center>
+      <a class="nav-link" href="#"><span class="material-icons md-light">info</span></a>
+    </center>
+  </div>
+</div>
+
+<nav class="navbar navbar-expand-sm navbar-dark fixed-bottom d-none d-lg-block" style="height: 60px; background-color: #4CAF50;">
+    <div class="navbar-nav container nav-pills nav-fill" style="padding: 0px;">
+      <div class="nav-item col">
+        <a class="nav-link" href="miEmpresa.php">Mi cuenta <span class="material-icons md-light btn">person</span></a>
+      </div> 
+      <div class="nav-item col">
+        <a class="nav-link" href="index.php">Inicio <span class="material-icons md-light btn">home</span></a>
+      </div> 
+      <div class="nav-item col">
+        <a class="nav-link" href="catalogo.php">Catalogo <span class="material-icons md-light btn">view_module</span></a>
+      </div> 
+      <div class="nav-item col">
+        <a class="nav-link" href="#">Info <span class="material-icons md-light btn">info</span></a>
+      </div> 
+    </div>
+</nav>
