9c77af665b6944bb06cfbca603c5c52188b64e7b
maurisba <mauriciobaa@alumno.hayas.edu.mx>
2020-04-23 18:07:35 -0500

    saque todo del folder new y lo meti a Venti

---
+++ inc/base.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<?php 
+		include 'inc/link.php';
+		include 'inc/navbar.php';
+?>
+<link rel="stylesheet" type="text/css" href="css/base.css">
+<body class="body">
+
+<div class="jumbotron" >
+		<div class="row align-items-center">
+			<span class="col-6 float-left"><center><h1 style="font-family: Pacifico; color: white;">MARQUET</h1></center></span>
+			<span class="col-6"><center><img src="../images/logo-marquet.png" alt="Logo"></center></span>
+		</div>
+
+</body>
