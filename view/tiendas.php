3057c78273424cd0d672d9e9ade661b7e5899e2d
HashMasterDx <lennynlean@hotmail.com>
2020-04-24 10:17:32 -0500

    productos

---
+++ view/tiendas.php
@@ -0,0 +1,93 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+	<link rel="stylesheet" type="text/css" href="css/base.css">
+	<?php 
+		include '../inc/link.php';
+	?>
+	<meta charset="UTF-8">
+	<title>Document</title>
+</head>
+<body>
+	<?php 
+		include '../inc/navbar.php';
+	 ?>
+	<div class="jumbotron">
+		<div class="row align-items-center">
+			<span class="col-6 float-left"><center><h1 style="font-family: Pacifico; color: white;">MARQUET</h1></center></span>
+			<span class="col-6"><center><img src="../images/logo-marquet.png" alt="Logo"></center></span>
+		</div>
+	</div>
+
+	<div class="container-fluid" style="">
+		<div class="container">
+			<div class="card text-center mb-3">
+				<div class="row no-gutters">
+					<div class="col-6 no-gutters">
+						<div class="row justify-content-center align-items-center" style="height: 100%;">
+							<div class="col">
+								<img class="img-fluid" src="https://miro.medium.com/max/720/1*nt-O8SIkJfmi7f_qnM8DGA.jpeg" alt="">
+							</div>
+						</div>
+					</div>
+					<div class="col-6 align-content-center">
+						<div class="row justify-content-center align-items-center" style="height: 100%;">
+							<div class="card-body">
+			        			<h3 class="card-title">TIENDA DON RAUL</h3>
+						        <p class="card-text">DESCRIPCION DE LA TIENDA.<br>Nos ubicamos en: avenida algo</p>
+						        <a class="btn btn-info" href="#">VER PRODUCTOS</a>
+						    </div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+
+		<div class="container">
+			<div class="card text-center mb-3">
+				<div class="row no-gutters">
+					<div class="col-6 no-gutters">
+						<div class="row justify-content-center align-items-center" style="height: 100%;">
+							<div class="col">
+								<img class="img-fluid" src="https://miro.medium.com/max/720/1*nt-O8SIkJfmi7f_qnM8DGA.jpeg" alt="">
+							</div>
+						</div>
+					</div>
+					<div class="col-6 align-content-center">
+						<div class="row justify-content-center align-items-center" style="height: 100%;">
+							<div class="card-body">
+			        			<h3 class="card-title">TIENDA DON RAUL</h3>
+						        <p class="card-text">DESCRIPCION DE LA TIENDA.<br>Nos ubicamos en: avenida algo</p>
+						        <a class="btn btn-info" href="#">VER PRODUCTOS</a>
+						    </div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+
+		<div class="container">
+			<div class="card text-center mb-3">
+				<div class="row no-gutters">
+					<div class="col-6 no-gutters">
+						<div class="row justify-content-center align-items-center" style="height: 100%;">
+							<div class="col">
+								<img class="img-fluid" src="https://miro.medium.com/max/720/1*nt-O8SIkJfmi7f_qnM8DGA.jpeg" alt="">
+							</div>
+						</div>
+					</div>
+					<div class="col-6 align-content-center">
+						<div class="row justify-content-center align-items-center" style="height: 100%;">
+							<div class="card-body">
+			        			<h3 class="card-title">TIENDA DON RAUL</h3>
+						        <p class="card-text">DESCRIPCION DE LA TIENDA.<br>Nos ubicamos en: avenida algo</p>
+						        <a class="btn btn-info" href="#">VER PRODUCTOS</a>
+						    </div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</body>
+</html>
