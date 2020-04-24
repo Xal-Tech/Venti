696e215a0a4ef86b1570d99f3dcc8e57a1e25ce2
HashMasterDx <lennynlean@hotmail.com>
2020-04-23 20:27:59 -0500

    Revert "form_registrar"
    This reverts commit 72b48c73aad45bace4de668c89ec8a2949f29a18.

--- view/registrar.php
+++ view/registrar.php
@@ -18,34 +18,5 @@
 			<span class="col-6"><center><img src="../images/logo-marquet.png" alt="Logo"></center></span>
 		</div>
 	</div>
-	<div class="container" style="margin-bottom: 80px;">
-	 	<div class="card" style="margin-bottom: 40px;">
-	 		<div class="card-title">
-	 			<center>
-	 			<strong><h2>Crea una cuenta</h2></strong>
-	 			<br>
-	 			<h5>Es rápido y sencillo</h5>
-	 			</center>
-	 		</div>
-	 	</div>
-	 	<div class="container-fluid">
-	 		<form  action="login/create-account.php" method="POST">
-	 			<div class="form-group">
-	 				<input type="text" class="form-control" name="username" placeholder="Nombre Completo">
-	 			</div>
-	 			<div class="form-group">
-	 				<input type="text" class="form-control" name="cellnumber" placeholder="Telefono celular">
-	 			</div>
-	            <div class="form-group">
-	            	<input type="password" class="form-control" name="password" placeholder="Contraseña">
-	            </div>
-	            <div class="form-group">
-	            	<input class="form-check-input" type="checkbox" id="terminos-y-condiciones-checkbox" required>
-	            	<label class="form-check-label">Aceptar <a href="#">términos y condiciones</a></label>
-	            </div>
-	            <button class="btn btn-info" type="submit" value="Crear cuenta">Crear cuenta</button>
-        	</form>
-	 	</div>
-	 </div>
 </body>
 </html>
