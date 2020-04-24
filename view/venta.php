f5ddd149bd1f741174e9d9bc9e901224ec3c4391
maurisba <mauriciobaa@alumno.hayas.edu.mx>
2020-04-24 11:11:52 -0500

    changes

--- view/venta.php
+++ view/venta.php
@@ -1,10 +1,11 @@
 <!DOCTYPE html>
 <html lang="es">
 <head>
-	<link rel="stylesheet" type="text/css" href="css/base.css">
+	<link rel="stylesheet" type="text/css" href="../css/base.css">
 	<?php 
 		include '../inc/link.php';
 	?>
+	<link rel="stylesheet" type="text/css" href="../css/venta.css">
 	<meta charset="UTF-8">
 	<title>Vender</title>
 </head>
@@ -18,12 +19,27 @@
 			<span class="col-6"><center><img src="../images/logo-marquet.png" alt="Logo"></center></span>
 		</div>
 	</div>
-	<div>Ingresar Producto</div>
-	<div id="div-producto">
-	<label>Nombre del producto</label><br>
-	<input type="text" class="form-control"  name="producto" placeholder="Nombre del producto">		
-	
-	
+	<center><div class="container-fluid" id="titulo">Ingresar Producto</div></center>
+	<div class="container-fluid" id="input-productos">
+	<label class="info">Nombre del producto</label><br>
+	<input type="text" class="form-control"  name="producto" placeholder="Nombre del producto">	<br>
+	<label class="info">Tipo de unidad para venta</label><br>
+	<div class="dropdown">
+	<button type="button" class="btn btn-primary dropdown-toggle" data-toggle="dropdown" >
+    Unidad 
+  </button>
+  <div class="dropdown-menu">
+    <a class="dropdown-item" href="#">Kg</a>
+    <a class="dropdown-item" href="#">g</a>
+	<a class="dropdown-item" href="#">Unidad</a>
+	<a class="dropdown-item" href="#">mL</a>
+	<a class="dropdown-item" href="#">L</a>
+  	</div>
+	</div><br>
+	<label class="info">Precio del producto por medida:</label><br>
+	<input type="text" class="form-control"  name="precio" placeholder="Precio/Medida"><br>
+	<label class="info">Sube la foto del producto</label><br>
+	<input type="file" id="input-imagen" name="imagen" class="buttons">
 	</div>
 
 
