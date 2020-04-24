f9bdb59807007177397fcfd8973504fa98036e80
HashMasterDx <lennynlean@hotmail.com>
2020-04-23 20:11:50 -0500

    Cambios de codigo base

--- inc/base.php
+++ inc/base.php
@@ -1,15 +1,22 @@
 <!DOCTYPE html>
-<?php 
-		include 'inc/link.php';
-		include 'inc/navbar.php';
-?>
-<link rel="stylesheet" type="text/css" href="css/base.css">
-<body class="body">
-
-<div class="jumbotron" >
+<html lang="es">
+<head>
+	<link rel="stylesheet" type="text/css" href="css/base.css">
+	<?php 
+		include 'link.php';
+	?>
+	<meta charset="UTF-8">
+	<title>Document</title>
+</head>
+<body>
+	<?php 
+		include 'navbar.php';
+	 ?>
+	<div class="jumbotron" >
 		<div class="row align-items-center">
 			<span class="col-6 float-left"><center><h1 style="font-family: Pacifico; color: white;">MARQUET</h1></center></span>
 			<span class="col-6"><center><img src="../images/logo-marquet.png" alt="Logo"></center></span>
 		</div>
-
+	</div>
 </body>
+</html>
