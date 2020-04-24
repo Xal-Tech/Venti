d62e2547c64f69a883b8bddf164800a6d88cdce3
maurisba <mauriciobaa@alumno.hayas.edu.mx>
2020-04-23 20:17:56 -0500

    hha

--- venta.php
+++ venta.php
@@ -1,22 +1,24 @@
 <!DOCTYPE html>
-<html>
-<title>Comprar</Title>
+<html lang="es">
 <head>
-<?php 
-		include 'inc/link.php';
-?>
-Vender
+	<link rel="stylesheet" type="text/css" href="css/base.css">
+	<?php 
+		include 'link.php';
+	?>
+	<meta charset="UTF-8">
+	<title>Vender</title>
 </head>
+
 <body>
-<?php 
-		include 'inc/navbar.php';
+	<?php 
+		include 'navbar.php';
 	 ?>
-<?php 
-		include 'inc/base.php';
-	 ?>
-     
-
-
+	<div class="jumbotron" >
+		<div class="row align-items-center">
+			<span class="col-6 float-left"><center><h1 style="font-family: Pacifico; color: white;">MARQUET</h1></center></span>
+			<span class="col-6"><center><img src="../images/logo-marquet.png" alt="Logo"></center></span>
+		</div>
+	</div>
 
 
 </body>
