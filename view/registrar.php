3a37681c7dc1d1aeebbaf445b70cd9c903ed8549
HashMasterDx <lennynlean@hotmail.com>
2020-04-23 20:19:47 -0500

    Cambio de carpetas

---
+++ view/registrar.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+	<link rel="stylesheet" type="text/css" href="css/base.css">
+	<?php 
+		include 'link.php';
+	?>
+	<meta charset="UTF-8">
+	<title>Document</title>
+</head>
+<body>
+	<?php 
+		include 'navbar.php';
+	 ?>
+	<div class="jumbotron" >
+		<div class="row align-items-center">
+			<span class="col-6 float-left"><center><h1 style="font-family: Pacifico; color: white;">MARQUET</h1></center></span>
+			<span class="col-6"><center><img src="../images/logo-marquet.png" alt="Logo"></center></span>
+		</div>
+	</div>
+	
+
+</body>
+</html>
