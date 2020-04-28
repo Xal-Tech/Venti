3e2fae48447a823c8eee20a710b25fac669cbfe1
Lennyn Lean <lennynlean@hotmail.com>
2020-04-27 23:41:36 -0500

    login

--- view/boc_login.php
+++ view/boc_login.php
@@ -5,19 +5,15 @@
 	<title>Login</title>
 	<link rel="stylesheet" type="text/css" href="css/style.css">
 	<?php 
-		include 'inc/link.php';
+		include '../inc/link.php';
 	 ?>
 </head>
 <body>
 	<?php 
-		include 'inc/navbar.php';
+		include '../inc/navbar.php';
+		include '../inc/jumbutron.php';
+		include '../inc/carrito.php'
 	 ?>
-	<div class="jumbotron" >
-		<div class="row align-items-center">
-			<span class="col-6 float-left"><center><h1 style="font-family: Pacifico; color: white;">MARQUET</h1></center></span>
-			<span class="col-6"><center><img src="../images/logo-marquet.png" alt="Logo"></center></span>
-		</div>
-	</div>
 	<div class="container" style="margin-bottom: 80px;">
 	 	<div class="card" style="margin-bottom: 40px;">
 	 		<div class="card-title">
